f22e52f450f3df55b461928a85b69e7472a6ba25
renjn <renjianan@aixuedai.com>
2017-10-16 11:16:19 +0800

    hh

--- app/src/main/java/com/qtfreet/watchactivity/MainActivity.java
+++ app/src/main/java/com/qtfreet/watchactivity/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends Activity implements OnCheckedChangeListener, O
 
     protected void onResume() {
         super.onResume();
-        status();
+        refrehStatus();
         NotificationActionReceiver.initNotification(this);
     }
 
@@ -86,43 +86,27 @@ public class MainActivity extends Activity implements OnCheckedChangeListener, O
 
     @Override
     public void onCancel(DialogInterface dialog) {
-
-
     }
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
-        status();
+        refrehStatus();
     }
 
-    private void status() {
+    private void refrehStatus() {
         mCompoundButton.setChecked(DefaultSharedPreferences.read(this));
 
     }
 
-    /**
-     * 检测辅助功能是否开启<br>
-     * 方 法 名：isAccessibilitySettingsOn <br>
-     * 创 建 人 <br>
-     * 创建时间：2016-6-22 下午2:29:24 <br>
-     * 修 改 人： <br>
-     * 修改日期： <br>
-     *
-     * @param mContext
-     * @return boolean
-     */
+
     private boolean isAccessibilitySettingsOn(Context mContext) {
         int accessibilityEnabled = 0;
-        // TestService为对应的服务
         final String service = getPackageName() + "/" + WatchingAccessibilityService.class.getCanonicalName();
         Log.i(TAG, "service:" + service);
-        // com.z.buildingaccessibilityservices/android.accessibilityservice.AccessibilityService
         try {
             accessibilityEnabled = Settings.Secure.getInt(mContext.getApplicationContext().getContentResolver(),
                     android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
-            Log.v(TAG, "accessibilityEnabled = " + accessibilityEnabled);
         } catch (Settings.SettingNotFoundException e) {
-            Log.e(TAG, "Error finding setting, default accessibility to not found: " + e.getMessage());
         }
         TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
 
@@ -130,15 +114,12 @@ public class MainActivity extends Activity implements OnCheckedChangeListener, O
             Log.v(TAG, "***ACCESSIBILITY IS ENABLED*** -----------------");
             String settingValue = Settings.Secure.getString(mContext.getApplicationContext().getContentResolver(),
                     Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
-            // com.z.buildingaccessibilityservices/com.z.buildingaccessibilityservices.TestService
             if (settingValue != null) {
                 mStringColonSplitter.setString(settingValue);
                 while (mStringColonSplitter.hasNext()) {
                     String accessibilityService = mStringColonSplitter.next();
 
-                    Log.v(TAG, "-------------- > accessibilityService :: " + accessibilityService + " " + service);
                     if (accessibilityService.equalsIgnoreCase(service)) {
-                        Log.v(TAG, "We've found the correct setting - accessibility is switched on!");
                         return true;
                     }
                 }
